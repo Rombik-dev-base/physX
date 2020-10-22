7a02c9fa4a3704f1fef3e81472899338beedeb57
Rombik-dev-base <61863339+Rombik-dev-base@users.noreply.github.com>
2020-10-22 13:53:55 +0300

    Add files via upload

---
+++ lab0/cmake-build-debug/CMakeFiles/Figures.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Figures.dir/main.cpp.obj"
+  "Figures.exe"
+  "Figures.exe.manifest"
+  "Figures.pdb"
+  "libFigures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Figures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
